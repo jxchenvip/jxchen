313b409a98319388331757d34f5cd88972890837
jxchen <jxchenvip@163.com>
2016-06-13 16:22:38 +0800

    update version add copy

--- bin/jxchen.js
+++ bin/jxchen.js
@@ -1,12 +1,13 @@
 #!/usr/bin/env node
 var fs = require('fs');
 var path = require('path');
-var dir = process.cwd();
-var dir_lib = path.join(__dirname, '../lib');
-var utils = path.join(__dirname, '../lib/utils');
-var color = require(path.join(utils, 'color'));
-var pkg = require(path.join('../', 'package.json'));
+var dir = process.cwd(); // dir 为全局变量初始为执行目录
+var dir_lib = path.join(__dirname, '../lib'); // 保存目录
+var utils = path.join(__dirname, '../lib/utils'); // 保存目录
+var color = require('../lib/utils/color'); // 设置颜色 
+var pkg = require('../package.json'); // 读取package.json
 var childProcess = require('child_process');
+var ncp = require("copy-paste"); // 复制，粘贴
 
 
 /**
@@ -16,6 +17,62 @@ if (process.argv[2] == '-V' || process.argv[2] == '-v') {
     console.log(pkg.version);
     process.exit();
 }
+
+
+/**
+ * 创建项目目录
+ */
+function mkdirSync() {
+    var cwd = process.cwd();
+    var newDir = path.relative(cwd, dir);
+    if (!fs.existsSync(newDir)) { // 检索当前目录是否有此文件夹
+        var pathtmp;
+        newDir.split(path.sep).forEach(function (dirname) {
+            if (pathtmp) {
+                pathtmp = path.join(pathtmp, dirname);
+            } else {
+                pathtmp = path.join(cwd, dirname);
+            }
+            if (!fs.existsSync(pathtmp)) fs.mkdir(pathtmp);
+        })
+    }
+    return true;
+}
+
+
+/**
+ * 拷贝文件
+ */
+function copy(from, to, chunk) {
+    var data = '';
+    var readerStream = fs.createReadStream(from);
+    readerStream.setEncoding('UTF8');
+    // 处理流事件 --> data, end, and error
+    readerStream.on('data', function (chunk) {
+        data += chunk;
+    });
+    readerStream.on('end', function () {
+        switch (chunk) { // 如果是grunt 需要相对压缩
+            case 4:
+                data = data.replace(/'all'/g, "'relative'");
+                break;
+        }
+        var writerStream = fs.createWriteStream(to);
+        writerStream.write(data, 'UTF8');
+        writerStream.end();
+        writerStream.on('finish', function () {
+            console.log(color.SUCCESS + path.basename(to) + '配置文件写入成功');
+            process_exit();
+        });
+        writerStream.on('error', function (err) {
+            console.log(color.ERROR + err.stack);
+        });
+    });
+    readerStream.on('error', function (err) {
+        console.log(color.ERROR + err.stack);
+    });
+}
+
 
 /**
  * 类似confirm
@@ -31,9 +88,9 @@ function prompt(prompt, callback) {
 }
 
 /**
- * 退出进程
+ * open dir
  */
-function process_exit(d) {
+function open_dir(d) {
     var cmd = '';
     if (process.platform === 'darwin') {
         cmd = 'open';
@@ -41,34 +98,36 @@ function process_exit(d) {
         cmd = 'start';
     }
     childProcess.execSync(cmd + ' ' + d);
+}
+
+
+/**
+ * 退出进程
+ */
+function process_exit() {
+    ncp.copy(dir.replace(/\\/g, '/')); // 复制当前路径
+    open_dir(dir); // 打开已创建目录
     process.exit();
 }
 
 /**
- * 创建项目目录
+ * 版本号++
  */
-function mkProjectDir() {
-    prompt('项目名称(project name) : ', selProject);
+function addVersion(c) {
+    c = c || '1.0.0';
+    while (fs.existsSync(path.join(dir, c))) {
+        c = (c.replace(/\./g, '') * 1 + 1).toString().replace(/\d{3}$/, function (a) {
+            return a.split('').join('.');
+        });
+    }
+    return c;
 }
 
-/**
- * 创建版本目录
- */
-function mkVersonDir() {
-    prompt('版本号(' + addVersion() + ') : ', createProjectVersion);
-}
-
-/**
- * 是否创建配制文件
- */
-function mkConfigFile() {
-    prompt('配制文件(NO) \n' + color.TIP + '1: webpack 2: gulp 3: grunt-all 4: grunt-relative : ', createConfig);
-}
 
 /**
  * 填写项目名称
  */
-function selProject(c) {
+function createProjectDir(c) {
     if (!/^[^\\/\\:\\*\\?\"<>\|]+$/.test(c)) {
         console.log(color.TIP + '项目名称只能为：数字、字母、下划线组合');
         mkProjectDir();
@@ -95,45 +154,26 @@ function selProject(c) {
     mkVersonDir(); // 创建版本目录
 }
 
-/**
- * 版本号++
- */
-function addVersion(c) {
-    c = c || '1.0.0';
-    while (fs.existsSync(path.join(dir, c))) {
-        c = (c.replace(/\./g, '') * 1 + 1).toString().replace(/\d{3}$/, function (a) {
-            return a.split('').join('.');
-        });
-    }
-    return c;
-}
 
 /**
- * 创建版本号
+ * 创建版本号文件夹
  */
-function createProjectVersion(c) {
+function createVersionDir(c) {
     if (!c || !/^\d+\.\d+\.\d+$/.test(c.trim())) {
         c = addVersion(c);
     }
     dir = path.join(dir, c);
-    moveFiles(dir);
-}
-
-/**
- * 创建项目文件，及配置文件
- */
-function moveFiles() {
-    mkdirSync(dir);
+    mkdirSync(); // 创建文件夹目录
     console.log(color.TIP + '版本目录已创建完成!');
     console.log(color.TIP + dir);
-    mkConfigFile(dir);
-    // createWebpackConfig(dir);
+    mkConfigFile(); // 创建配制文件
 }
+
 
 /**
  * 选择项目所需要的配置文件也可以为空
  */
-function createConfig(c) {
+function createConfigFile(c) {
     c = c || '';
     switch (c) {
         case '1':
@@ -151,109 +191,30 @@ function createConfig(c) {
             break;
         default:
             console.log(color.SUCCESS + '项目目录创建完成,无配置文件！');
-            process_exit(dir);
+            process_exit();
             break;
     }
 }
 
 /**
- * 拷贝文件
- */
-function copy(from, to, chunk) {
-    var data = '';
-    var readerStream = fs.createReadStream(from);
-    readerStream.setEncoding('UTF8');
-    // 处理流事件 --> data, end, and error
-    readerStream.on('data', function (chunk) {
-        data += chunk;
-    });
-    readerStream.on('end', function () {
-        switch (chunk) { // 如果是grunt 需要相对压缩
-            case 4:
-                data = data.replace(/'all'/g, "'relative'");
-                break;
-        }
-        var writerStream = fs.createWriteStream(to);
-        writerStream.write(data, 'UTF8');
-        writerStream.end();
-        writerStream.on('finish', function () {
-            console.log(color.SUCCESS + path.basename(to) + '配置文件写入成功');
-            process_exit(dir);
-        });
-        writerStream.on('error', function (err) {
-            console.log(color.ERROR + err.stack);
-        });
-    });
-    readerStream.on('error', function (err) {
-        console.log(color.ERROR + err.stack);
-    });
-}
-
-/**
  * 创建项目目录
  */
-function mkdirSync() {
-    var cwd = process.cwd();
-    var newDir = path.relative(cwd, dir);
-    if (!fs.existsSync(newDir)) { // 检索当前目录是否有此文件夹
-        var pathtmp;
-        newDir.split(path.sep).forEach(function (dirname) {
-            if (pathtmp) {
-                pathtmp = path.join(pathtmp, dirname);
-            } else {
-                pathtmp = path.join(cwd, dirname);
-            }
-            if (!fs.existsSync(pathtmp)) fs.mkdir(pathtmp);
-        })
-    }
-    return true;
+function mkProjectDir() {
+    prompt('项目名称(project name) : ', createProjectDir);
 }
 
 /**
- * 猜测根目录
+ * 创建版本目录
  */
-function findRootDir() {
-    var d = path.join(__dirname);
-    var flag = true;
-    while (flag) {
-        var dir = fs.readdirSync(d).reduce(function (o, key) {
-            o[key] = path.join(d, key);
-            return o;
-        }, {});
-        if (dir['node_modules'] && dir['webpack.config.js'] && dir['package.json']) {
-            flag = false;
-        } else {
-            d = path.join(d, '../');
-        }
-    }
-    return d;
+function mkVersonDir() {
+    prompt('版本号(' + addVersion() + ') : ', createVersionDir);
 }
 
 /**
- * 创建配置文件
+ * 是否创建配制文件
  */
-function createWebpackConfig(c) {
-    var cname = 'webpack.config.js';
-    var rootdir = findRootDir(); // 猜测根目录 [node_module, package, webpack.config.js]三个文件同时存在的目录
-    fs.writeFile(path.join(c, cname), settingConfig(rootdir), function (err, stdout, stdin) {
-        if (err) {
-            console.log(color.ERROR + '写入' + cname + '出错');
-        } else {
-            console.log(color.SUCCESS + '配置文件写入成功！');
-            console.log(path.join(dir, cname));
-            process.exit();
-        }
-    })
-}
-
-/**
- * 配置文件里面
- */
-function settingConfig(rootdir) {
-    var s = '';
-    s += 'var conf = require("' + rootdir.replace(/\\/g, '/') + 'webpack.config.js");\n';
-    s += 'module.exports = conf(__dirname, "' + rootdir.replace(/\\/g, '/') + '");\n';
-    return s;
+function mkConfigFile() {
+    prompt('配制文件(NO) \n' + color.TIP + '1: webpack 2: gulp 3: grunt-all 4: grunt-relative : ', createConfigFile);
 }
 
 // console.log('***      module.filename = ' + module.filename + ' ***');
